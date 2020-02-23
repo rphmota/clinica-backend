2309a729d4e17664ae9e454c8eae41bb1cab2b33
Raphael <rphmota@gmail.com>
2020-02-23 02:46:54 -0300

    Comentarios
    add comentarios no codigo e importação do banco de dados

--- src/app.js
+++ src/app.js
@@ -1,5 +1,14 @@
+/**
+ * Temos uma classe inicialmente importamos o express e o arquivo de rotas
+ * temos as variaveis de instancia e os metodos
+ * metodo middlewares tem apenas a utilização do json para ser tratado nas requisições
+ * e o metodo routes que usa as rotas importadas do arquivo de rotas como parametro na funcao use
+ * do express
+ */
 const express = require('express')
 const routes = require('./routes')
+
+const database = require('./database/index')
 
 class App {
     constructor() {
