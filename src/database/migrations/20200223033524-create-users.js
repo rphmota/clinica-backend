974568698957eef102f7281f551b219018b077d2
Raphael <rphmota@gmail.com>
2020-02-23 02:47:51 -0300

    Migration
    add migration de usuario

---
+++ src/database/migrations/20200223033524-create-users.js
@@ -0,0 +1,62 @@
+/**
+ * Arquivos d migrations consistem em criações 
+ * de tabelas no banco de dados o metodo up cria
+ * a tabela e o metodo down faz o rollback
+ * O comando para rodar a migrate e yarn sequelize db:migrate
+ * caso eu queira desfazer a migration eu rodo yarn sequelize db:migrate:undo
+ * db:migrate:undo:all desfaz tudo
+ */
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('users', { 
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+      },
+      password_hash: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      access_level: {
+        type: Sequelize.STRING(1),
+        allowNull: false,
+        defaultValue: 'C'
+      },
+      cpf: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+      },
+      phone: {
+        type: Sequelize.STRING,
+        allowNull: false  
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      } 
+    });
+    
+  },
+
+  down: (queryInterface, Sequelize) => {    
+    return queryInterface.dropTable('users');    
+  }
+};
