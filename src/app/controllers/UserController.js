4a582b1e9a5a83f49905103fcc0e56614984e0f4
Raphael Mota <rphmota@gmail.com>
2020-04-02 11:02:17 -0300

    Update UserController.js
    validacoes e listagem de usuarios

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -6,11 +6,36 @@
  * No metodo save eu faco a primeira verificacao se ja existe o email ou cpf na base caso exista ele retorna um erro 
  * Quando de fato eu cadastro um usuario na base seria melhor retornar para o front apenas alguns dados desse usuario cadastrado
  * portando o retorno do create apenas algumas informacoes estao vindo do retorno
+ * Validacoes feitas com YUP declaro uma variavel schema e depois digo que um objeto
+ * YUP tem as seguintes caracteristicas(sahpe), apos isso verifico se esta valido o que foi passado no body
+ * com o metodo isValid caso a validacao nao passe retorno um json com erro. Nas validacoes podemos utilizar
+ * condicionais pois o yup permite observamos tais condicionais no metodo edit
+ * No yup temos o .when() que e quando eu quero fazer um condicional em cima de qq outra variavel declarada eanteriormente
+ * assim o segundo parametro e uma funcao que o primeiro parametro e a propria variavel e o segundo parametro e
+ * a continuacao da validacao do proprio campo em questao
  */
 const Yup = require('yup')
  const User = require('../models/User')
 class UserController {
-    async save(req,res) {        
+
+    async list(req,res) {
+        const users = await User.findAll()
+        
+        res.status(200).json({users})
+    }
+
+    async save(req,res) {
+        const schema = Yup.object().shape({
+            cpf: Yup.string().required(),
+            name: Yup.string().required(),
+            access_level: Yup.string(),
+            password: Yup.string().required().min(6)                
+        })     
+        
+        if(!(await schema.isValid(req.body))) {
+            res.status(400).json({erro: 'falha na validacao'})
+        }
+        
         const cpfExists = await User.findOne({where: {cpf: req.body.cpf}})        
         if(cpfExists) {
             return res.status(400).json({Msg: 'CPF ja cadastrado na base de dados'})
@@ -24,9 +49,22 @@ class UserController {
         }
     }
     async edit(req,res) {
+       const schema = Yup.object().shape({
+           login: Yup.string().required(),
+           name: Yup.string(),
+           access_level: Yup.string(),
+           oldPassword: Yup.string().min(6),  
+           password: Yup.string().min(6).when('oldPassword',(oldPassword, field) => {
+                oldPassword ? field.required() : field 
+           })
+       })     
+        
+       if(!(await schema.isValid(req.body))) {
+           res.status(400).json({erro: 'falha na validacao'})
+       }
        const {login,oldPassword} = req.body
 
-       const user = await User.findByPk(req.userId_token)
+       const user = await User.findOne({where: {cpf: req.body.login}})
 
        if(login!=user.cpf) {
            const userExists = await User.findOne({where: {cpf: login}})
@@ -34,7 +72,7 @@ class UserController {
             return   res.status(400).json({error: "Usuario ja existe"})
            }
        }
-       if(oldPassword && !(await user.checkPassword(oldPassword))) {
+       if(oldPassword && !(await user.checkPassword(oldPassword))) {                             
            return   res.status(401).json({error: "Senha nao confere"}) 
        }
 
@@ -46,7 +84,7 @@ class UserController {
                 id,
                 name,
                 cpf,
-                access_level
+                access_level                
             }
             )
     }
